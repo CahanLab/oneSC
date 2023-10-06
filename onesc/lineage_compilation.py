44ca24340eb2bfd9ec77cee6c4c2e8f75eff4872
dpeng28 <dpeng5@jh.edu>
2023-10-06 17:36:18 -0400

    added documentation for lineage_compilation.py

--- onesc/lineage_compilation.py
+++ onesc/lineage_compilation.py
@@ -3,67 +3,110 @@ import pandas as pd
 import itertools
 import networkx as nx 
 
-
 def find_gene_change_trajectory(train_exp, train_st, trajectory_cells_dict, cluster_col, pt_col, bool_thresholds, pseudoTime_bin, smooth_style = 'mean'):
+    """Identify the approximate pseudotime at which the genes change status (on to off or off to on) along each trajectory. 
+    This information will be used to help curate the training data for genetic algorithm onesc.curate_training_data(). 
+
+
+    Args:
+        train_exp (pandas.DataFrame): Single-cell expression table. 
+        train_st (pandas.DataFrame): Sample table for the expression table. 
+        trajectory_cells_dict (dict): Output from onesc.extract_trajectory(). It is a dictionary that categorizes different cell states into different trajectory. 
+                                      The name of the dictionary is the trajectory name and the item of the dictionary is a list of cell state. 
+        cluster_col (str): The column name of the column in train_st containing cell states or clusters information. 
+        pt_col (str): The column name of the column in train_st containing psedotime ordering. 
+        bool_thresholds (pandas.series): Output from onesc.find_threshold_vector. It contains the threshold value (between on and off) for each gene. The indexes are the gene names and the values are the threshold expression values. 
+        pseudoTime_bin (float): The pseudotime resolution at which we bin the cells within and average the expressions. 
+        smooth_style (str, optional): The smoothing style ('mean' or 'median'). Defaults to 'mean'.
+
+    Returns:
+        dict: A dictionary of pandas dataframes documenting the pseudotime point at which the genes change status across a trajectory. The keys of the dictionary represent the trajectory names, and the items are pandas dataframe document the pseudotime point at which genes change status. 
+    """
     trajectory_time_change_dict = dict()
     for temp_trajectory in trajectory_cells_dict.keys(): 
-        activation_time = find_genes_time_change(train_st, train_exp, trajectory_cells_dict, cluster_col, pt_col, bool_thresholds, pseudoTime_bin, temp_trajectory)
+        activation_time = find_genes_time_change(train_st, train_exp, trajectory_cells_dict, cluster_col, pt_col, bool_thresholds, pseudoTime_bin, temp_trajectory, smooth_style = smooth_style)
         trajectory_time_change_dict[temp_trajectory] = activation_time
     return trajectory_time_change_dict
 
 def find_genes_time_change(train_st, train_exp, trajectory_cluster_dict, cluster_col, pt_col, vector_thresh, pseudoTime_bin, trajectory_name, smooth_style = 'mean'):
+    """Find the pseudotime point at which the genes change status (on to off or off to on) along one trajectory. This is a helper function for onesc.find_gene_change_trajectory
+
+    Args:
+        train_st (pandas.DataFrame): Sample table for the expression table. 
+        train_exp (pandas.DataFrame): Single-cell expression table. 
+        trajectory_cells_dict (dict): Output from onesc.extract_trajectory(). It is a dictionary that categorizes different cell states into different trajectory. 
+                                      The name of the dictionary is the trajectory name and the item of the dictionary is a list of cell state. 
+        cluster_col (str): The column name of the column in train_st containing cell states or clusters information. 
+        pt_col (str): The column name of the column in train_st containing psedotime ordering. 
+        vector_thresh (pandas.series): Output from onesc.find_threshold_vector. It contains the threshold value (between on and off) for each gene. The indexes are the gene names and the values are the threshold expression values. 
+        pseudoTime_bin (float): The pseudotime resolution at which we bin the cells within and average the expressions. 
+        trajectory_name (str): The name of the trajectory. 
+        smooth_style (str, optional): The smoothing style ('mean' or 'median'). Defaults to 'mean'.
+
+    Returns:
+        pandas.DataFrame: pandas dataframe document the pseudotime point at which genes change status.
+    """
     target_clusters = trajectory_cluster_dict[trajectory_name]
     sub_train_st = train_st.loc[train_st[cluster_col].isin(target_clusters), :]
     sub_train_st = sub_train_st.sort_values(pt_col)
     sub_train_exp = train_exp.loc[:, sub_train_st.index]
-
     activation_time_df = pd.DataFrame()
     for temp_gene in sub_train_exp.index: 
         time_series = pd.DataFrame()
         time_series['expression'] = sub_train_exp.loc[temp_gene, :]
         time_series['PseudoTime'] = sub_train_st[pt_col]
-
-        smoothed_series = bin_smooth(time_series, pseudoTime_bin, smooth_style = smooth_style, spline_ME = 0.1)
+        smoothed_series = bin_smooth(time_series, pseudoTime_bin, smooth_style = smooth_style)
         smoothed_series['boolean'] = smoothed_series['expression'] > vector_thresh[temp_gene]
-
         temp_change_df = find_transition_time(smoothed_series, temp_gene)
         activation_time_df = pd.concat([activation_time_df, temp_change_df])
-    
     activation_time_df = activation_time_df.sort_values("PseudoTime")
     activation_time_df.index = list(range(0, activation_time_df.shape[0]))
     return activation_time_df
 
-# this function is to find time points across the dataset 
 def find_transition_time(smoothed_series, temp_gene):
-    current_status = smoothed_series.loc[0, 'boolean']
+    """Find the transition pseudotime point. This is a helper function for onesc.find_genes_time_change()
+
+    Args:
+        smoothed_series (pandas.DataFrame): pandas dataframe containing smoothed expression values for a particular gene at different pseudotime interval. 
+        temp_gene (str): the name of the gene 
+
+    Returns:
+        pandas.DataFrame: pandas dataframe documenting all the instance at which the temp_gene changed across pseudotime. 
+    """
+    current_status = smoothed_series.loc[0, 'boolean'] # identify the inital state of the gene 
     transition_df = pd.DataFrame(index = ['PseudoTime', 'gene', 'type'])
-    
     lag = 5
-    # if the initial point is activated 
     if current_status == True: 
         transition_df = pd.concat([transition_df, pd.Series({'PseudoTime': 0, 'gene': temp_gene, 'type': '+'})], axis = 1)
     elif current_status == False: 
         transition_df = pd.concat([transition_df, pd.Series({'PseudoTime': 0, 'gene': temp_gene, 'type': '-'})], axis = 1)
-
     for index in smoothed_series.index[0:len(smoothed_series.index)-lag]: 
         if smoothed_series.loc[index, 'boolean'] == current_status:
             continue
         else: 
             if np.sum(smoothed_series.loc[index:index+lag, 'boolean']) == 0 or np.sum(smoothed_series.loc[index:index+lag, 'boolean']) == len(smoothed_series.loc[index:index+lag, 'boolean']):
+                # identify whether if there is a positive change or negative change
                 if current_status == False and smoothed_series.loc[index, 'boolean'] == True: 
                     reg_type = "+"
                 elif current_status == True and smoothed_series.loc[index, 'boolean'] == False: 
                     reg_type = "-"
-            
                 transition_df = pd.concat([transition_df, pd.Series({'PseudoTime': smoothed_series.loc[index, 'PseudoTime'], 'gene': temp_gene, 'type': reg_type})], axis = 1)
                 current_status = smoothed_series.loc[index, 'boolean']
-    
     transition_df = transition_df.T
     transition_df.index = list(range(0, transition_df.shape[0]))
-  
     return transition_df
 
-def bin_smooth(time_series, pseudoTime_bin, smooth_style = "mean", spline_ME = 0.1):
+def bin_smooth(time_series, pseudoTime_bin, smooth_style = "mean"):
+    """Bin average the expression across pseudotime bins. This is a helper function for onesc.find_genes_time_change
+
+    Args:
+        time_series (pandas.DataFrame): Dataframe with one column representing pseudotime and other column representing the expression of a cell. 
+        pseudoTime_bin (float): Bin size for pseudotime. 
+        smooth_style (str, optional): Whether to get the 'mean' of the expression within pseudotime bin or 'median'. Defaults to "mean".
+
+    Returns:
+        pandas.DataFrame: Dataframe with bin averged or medianed expressions across different pseudotime segments. 
+    """
     curr_time =  np.min(time_series['PseudoTime'])
     time_list = list()
     smoothed_exp = list()
@@ -86,20 +129,26 @@ def bin_smooth(time_series, pseudoTime_bin, smooth_style = "mean", spline_ME = 0
         curr_time = curr_time + pseudoTime_bin
         stand_dev.append(temp_time_series['expression'].std())
 
-    # spline_w = np.divide(1, stand_dev)
-
     smoothed_data = pd.DataFrame()
     smoothed_data['PseudoTime'] = time_list
     smoothed_data['expression'] = smoothed_exp
-
-    #spline_s = smoothed_data.shape[0] * (spline_ME ** 2)
-    #spline_xy = UnivariateSpline(smoothed_data['PseudoTime'],smoothed_data['expression'], s = spline_s)
-    #moothed_data['splined_exp'] = spline_xy(smoothed_data['PseudoTime'])
     return smoothed_data 
 
-# this function is to find the threshold of the genes based on the data observation 
-# this could obviously be a little bit better 
 def find_threshold_vector(exp_df, samp_st, cluster_col = "cluster", cutoff_percentage = 0.4): 
+    """Identify the expression threshold for each gene. Compile them in a series. 
+       First, it calculates the average expression for each cell state or cluster. 
+       Then, it identifies the highest averge expression and lowest expression. 
+       cutoff_percentage * the difference between highest and lowest average expression would be the expression threshold for the gene. 
+
+    Args:
+        exp_df (pandas.DataFrame): The expression matrix. 
+        samp_st (pandas.DataFrame): The sample table for the single-cell expression matrix. 
+        cluster_col (str, optional): The column name for the column containing the cluster information. Defaults to "cluster".
+        cutoff_percentage (float, optional): The minimum percent cut-off of the difference between highest and lowest average experssion. Defaults to 0.4.
+
+    Returns:
+        pandas.Series: Series of expression threshold for the genes. 
+    """
     cluster_exp = pd.DataFrame()
     for temp_cluster in np.unique(samp_st[cluster_col]):
         temp_st = samp_st.loc[samp_st[cluster_col] == temp_cluster, :]
@@ -108,6 +157,21 @@ def find_threshold_vector(exp_df, samp_st, cluster_col = "cluster", cutoff_perce
     return ((cluster_exp.max(axis = 1) - cluster_exp.min(axis = 1)) * cutoff_percentage + cluster_exp.min(axis = 1))
 
 def construct_cluster_network(train_exp, sampTab, initial_clusters, terminal_clusters, cluster_col = "cluster_id", pseudo_col = "pseudotime"):
+    """Construct a graph connecting the cell states or clusters transitions based on expression similarities and pseudotime ordering (lower pseudotime suggests higher upstream). 
+       This function typically works the best when the transitional relationships are not cyclical (ex A->B->C->A). 
+       If in any event where this function cannot create the cluster network, the user can also manually create a networkx directed graph object. 
+
+    Args:
+        train_exp (pandas.DataFrame): The single-cell expression table. 
+        sampTab (pandas.DataFrame): The sample table for the single-cell expression table. 
+        initial_clusters (list): A list of user defined initial cell states or clusters. 
+        terminal_clusters (list): A list of user defined terminal cell states or clusters. 
+        cluster_col (str, optional): The column name for column containing cell states information. Defaults to "cluster_id".
+        pseudo_col (str, optional): The column name for column containing pseudotime information. Defaults to "pseudotime".
+
+    Returns:
+        networkx.DiGraph: A networkx directed graph object summarizing the transitional relationship between cell states. 
+    """
     pt_list = list()
     cluster_list = list()
     mean_exp = pd.DataFrame()
@@ -168,9 +232,18 @@ def construct_cluster_network(train_exp, sampTab, initial_clusters, terminal_clu
     return my_G 
 
 def extract_trajectory(clusters_G, initial_clusters, terminal_clusters): 
+    """Decomposed the networkx dircted graph object into a individual trajectories. The key of the dictionary is the trajectory name, and the items are lists of clusters inside that trajectory. 
+
+    Args:
+        clusters_G (networkx.DiGraph): Networkx directed graph object from onesc.construct_cluster_network or manually curated. 
+        initial_clusters (list): A list of user defined initial cell states or clusters. 
+        terminal_clusters (list): A list of user defined terminal cell states or clusters. 
+
+    Returns:
+        dict: Dictionary of different trajectories and the clusters associated to the individual trajectory. 
+    """
     clusters_trajectory_dict = dict()
     i = 0 
-
     start_end_combos = itertools.product(initial_clusters, terminal_clusters)
     for unique_combo in start_end_combos:
         for item in nx.all_simple_paths(clusters_G, unique_combo[0], unique_combo[1]): 
