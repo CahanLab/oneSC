f85bf3b3a60f6c45aecc3a86a7c4c3365e75d993
dpeng28 <dpeng5@jh.edu>
2024-06-04 23:00:47 -0400

    update dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="onesc", 
-    version="0.0.1",
+    version="0.1.1",
     author="Dan Peng",
     author_email="dpeng5@jhu.edu",
     description="This is a prototype for OneSC",
@@ -22,15 +22,15 @@ setuptools.setup(
         'click',
         'pygad>=3.1.0',
         'pygam>=0.8.0',
-        'numpy>=1.21.5', 
+        'numpy>=1.21.5, <1.28.0', 
         'pandas>=1.4.3',
         'statsmodels>=0.13.2', 
-        'scipy>=1.8.0', 
+        'scipy>=1.8.0, <1.12.0', 
         'scanpy>=1.9.1', 
         'joblib>=1.1.0',
-        'networkx>=2.8.8',
-        'adjustText', 
-        'igraph'
+        'networkx>=2.8.8, <=3.2.1',
+        'adjustText>=1.1.1', 
+        'igraph>=0.11.5'
     ],
     python_requires='>=3.9',
 )
